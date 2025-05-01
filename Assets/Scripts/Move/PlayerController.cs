7ee3c981b1dd801c720476a81247eb1f52cea267
Renew023 <ya1539@naver.com>
2025-05-01 10:07:48 +0900

    feat. 플레이어 상태에 따라 움직이는 컨트롤러 정의
    + 플레이어 상태 값 지정

---
+++ Assets/Scripts/Move/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,
+    Move,
+    Jump,
+    Die
+}
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
